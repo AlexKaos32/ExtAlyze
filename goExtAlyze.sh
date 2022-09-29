0a14b39fd76904fd3e14403421db1816014642c2
Alex Kaos <59406285+AlexKaos32@users.noreply.github.com>
2022-09-29 09:46:45 -0500

    Update goExtAlyze.sh

--- goExtAlyze.sh
+++ goExtAlyze.sh
@@ -25,7 +25,7 @@ printf "\n"
 
 printf "\nStarting service discovery\n"
 
-nmap --min-rate=150 --min-hostgroup=25 --max-retries=2 --initial-rtt-timeout=250ms --max-rtt-timeout=400ms --max-scan-delay=5 -Pn -sS -sV  -O -sU -p T:1-65535,U:53,67-69,111,123,135,137-139,161-162,445,500,514,520,623,631,996-999,1434,1701,1900,3283,4500,5353,49152-49154 -iL $ip -vv -oA $userDir/$client/$out
+nmap --min-rate=150 --min-hostgroup=25 --max-retries=2 --initial-rtt-timeout=250ms --max-rtt-timeout=400ms --max-scan-delay=50ms -Pn -sS -sV  -O -sU -p T:1-65535,U:53,67-69,111,123,135,137-139,161-162,445,500,514,520,623,631,996-999,1434,1701,1900,3283,4500,5353,49152-49154 -iL $ip -vv -oA $userDir/$client/$out
 
 while true; do
 	printf "Do you want to run Gowitness? "
